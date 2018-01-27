9d5c6e277cf2eefe80c49b92c98119cc7263622b
begimov <begimov@gmail.com>
2018-01-27 15:20:30 +0300

    Example component imported and used in App component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
     <div id="app">
-
+        <example />
     </div>
 </template>
 
 <script>
-    //
+    import Example from './app/Example.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            Example
+        }
+    }
 </script>
